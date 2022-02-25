b6dbbdfa4318ffc34ad15e1297f1ff3d0341bca0
Billcountry <me@billcountry.tech>
2022-02-25 18:36:53 +0300

    Encode and test encode

--- encode.go
+++ encode.go
@@ -1,1 +1,100 @@
 package eson
+
+import (
+	"encoding/json"
+	"github.com/alt4dev/eson/extension"
+	"reflect"
+)
+
+func Encode(goObject interface{}) (string, error) {
+	goObject = preProcess(goObject)
+
+	resp, err := json.Marshal(goObject)
+	if err != nil {
+		return "", err
+	}
+	return string(resp), nil
+}
+
+func preProcess(goObject interface{}) (processedObject interface{}) {
+	objectValue := reflect.ValueOf(goObject)
+	if objectValue.Type().Kind() == reflect.Ptr {
+		objectValue = reflect.Indirect(objectValue)
+	}
+
+	processedObject = goObject
+
+	switch objectValue.Type().Kind() {
+	case reflect.Map:
+		processedObject = processMap(goObject.(map[string]interface{}))
+		break
+	case reflect.Struct:
+		processedObject = processStruct(goObject)
+		break
+	case reflect.Array:
+	case reflect.Slice:
+		goArray := make([]interface{}, 0)
+		for i := 0; i < objectValue.Len(); i++ {
+			goArray = append(goArray, objectValue.Index(i).Interface())
+		}
+		processedObject = processArray(goArray)
+		break
+	}
+	return processedObject
+}
+
+func processMap(theMap map[string]interface{}) interface{} {
+	encoded := make(map[string]interface{})
+	for key, value := range theMap {
+		encodedKey, encodedValue := extension.EncodeValue(key, value)
+		if key == encodedKey {
+			// Preprocess the value
+			encodedValue = preProcess(value)
+		}
+		encoded[encodedKey] = encodedValue
+	}
+	return encoded
+}
+
+func processStruct(theStruct interface{}) interface{} {
+	encoded := make(map[string]interface{})
+	structType := reflect.TypeOf(theStruct)
+	structValue := reflect.ValueOf(theStruct)
+
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		// Ignore non-exported fields
+		if !field.IsExported() {
+			continue
+		}
+
+		key := field.Name
+		if jsonTag, ok := field.Tag.Lookup("json"); ok {
+			if jsonTag == "-" {
+				continue
+			}
+			if jsonTag != "" {
+				key = jsonTag
+			}
+		}
+
+		value := structValue.FieldByName(field.Name)
+		encoded[key] = value.Interface()
+	}
+	return processMap(encoded)
+}
+
+func processArray(theArray []interface{}) interface{} {
+	arrayOut := make([]interface{}, len(theArray))
+	for i, value := range theArray {
+		key, encodedValue := extension.EncodeValue("", value)
+		if key != "" {
+			encodedValue = map[string]interface{}{key: encodedValue}
+		} else {
+			// Preprocess the value
+			encodedValue = preProcess(encodedValue)
+		}
+		arrayOut[i] = encodedValue
+	}
+	return arrayOut
+}
--- encode_test.go
+++ encode_test.go
@@ -1,1 +1,60 @@
 package eson
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type testStruct struct {
+	Name       string    `json:"name"`
+	DOB        time.Time `json:"date_of_birth"`
+	Roles      []string  `json:"roles"`
+	Registered time.Time
+}
+
+func TestEncode(t *testing.T) {
+	data := testStruct{
+		Name:       "Jane Doe",
+		DOB:        time.Now(),
+		Roles:      []string{"admin", "client"},
+		Registered: time.Now(),
+	}
+
+	encodedData, err := Encode(data)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	expectedOutput := fmt.Sprintf(`{"EsonDatetime~Registered":%v,"EsonDatetime~date_of_birth":%v,"name":"Jane Doe","roles":["admin","client"]}`, data.Registered.UnixMilli(), data.DOB.UnixMilli())
+
+	if expectedOutput != encodedData {
+		t.Error("UnExpected JSON output")
+		t.Error("Expected:", expectedOutput)
+		t.Error("Found:   ", encodedData)
+		return
+	}
+}
+
+func TestEncodeList(t *testing.T) {
+	time1 := time.Now()
+	time2 := time.Now().Add(time.Second * 10)
+	time3 := time.Now().Add(time.Second * 30)
+
+	times := []time.Time{time1, time2, time3}
+	encodedData, err := Encode(times)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	expectedOutput := fmt.Sprintf(`[{"EsonDatetime~":%v},{"EsonDatetime~":%v},{"EsonDatetime~":%v}]`, time1.UnixMilli(), time2.UnixMilli(), time3.UnixMilli())
+
+	if expectedOutput != encodedData {
+		t.Error("UnExpected JSON output")
+		t.Error("Expected:", expectedOutput)
+		t.Error("Found:   ", encodedData)
+		return
+	}
+}
